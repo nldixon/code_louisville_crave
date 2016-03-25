7f0fa763320298667467d5b9c43cd8c8537d1170
Nicholas Dixon <nldixon@gmail.com>
2016-03-25 13:52:08 +0000

    JS updates.

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $.ajax({
           a.append(img);
           $(".sandwichGallery").append(a);
         });
-        $(".sandwichGallery").justifiedGallery(3);
+        $(".sandwichGallery").justifiedGallery();
     }
 });
 })
